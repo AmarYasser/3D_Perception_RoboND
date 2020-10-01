566cbb053c3e15b8978855b9327d33d4b307beb4
AmarYasser <amaryaser46@gmail.com>
2020-09-30 17:50:23 -0700

    3D Perception project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/asd/Downloads/3D_Perception/src
+# Build directory: /home/asd/Downloads/3D_Perception/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(gazebo_grasp_plugin)
+subdirs(pr2_robot)
+subdirs(pr2_moveit)
---
+++ build/pr2_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/asd/Downloads/3D_Perception/src/pr2_robot
+# Build directory: /home/asd/Downloads/3D_Perception/build/pr2_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
